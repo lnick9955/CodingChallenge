4a70892c406b9ef803165843d3e80a44464afc56
Nick Lane <lnick9955@gmail.com>
2023-09-20 08:58:29 -0500

    apply solutions

--- CodingChallenge.FamilyTree/Solution.cs
+++ CodingChallenge.FamilyTree/Solution.cs
@@ -6,7 +6,28 @@ namespace CodingChallenge.FamilyTree
     {
         public string GetBirthMonth(Person person, string descendantName)
         {
-            throw new NotImplementedException();
+            // Check if the current person's name matches the target descendant's name
+            // This check must be at the top of the function because this is
+            // an exit condition for recursive function calls.
+            if (person.Name == descendantName)
+            {
+                return person.Birthday.ToString("MMMM");
+            }
+
+            // Recursively search through the descendants
+            foreach (var decendent in person.Descendants)
+            {
+                // Recursively call the function on each descendant to search for the target name
+                var birthMonth = GetBirthMonth(decendent, descendantName);
+
+                // If a non-empty birth month is found in the descendants, propagate it up the tree
+                if (birthMonth != "")
+                {
+                    return birthMonth;
+                }
+            }
+
+            return "";
         }
     }
 }
--- CodingChallenge.PirateSpeak/Solution.cs
+++ CodingChallenge.PirateSpeak/Solution.cs
@@ -1,13 +1,68 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace CodingChallenge.PirateSpeak
 {
     public class Solution
     {
+        // Get character occurences for a word
+        public Dictionary<char, int> getWordCharacters(string word)
+        {
+            Dictionary<char, int> wordChars = new Dictionary<char, int>();
+            foreach (char c in word)
+            {
+                if (wordChars.ContainsKey(c))
+                {
+                    wordChars[c] += 1;
+                }
+                else
+                {
+                    wordChars.Add(c, 1);
+                }
+            }
+            return wordChars;
+        }
+
         public string[] GetPossibleWords(string jumble, string[] dictionary)
         {
-            throw new NotImplementedException();
+            List<string> possibleWords = new List<string>();
+
+            var jumbleChars = this.getWordCharacters(jumble);
+
+            // Iterate through each word in the dictionary
+            foreach (string word in dictionary)
+            {
+                if (word.Length != jumble.Length)
+                {
+                    continue;
+                }
+
+                Dictionary<char, int> wordChars = new Dictionary<char, int>();
+                int i;
+                for (i = 0; i < word.Length; i ++)
+                {
+                    char c = word[i];
+                    if (wordChars.ContainsKey(c))
+                    {
+                        wordChars[c] += 1;
+                    }
+                    else
+                    {
+                        wordChars.Add(c, 1);
+                    }
+                    if (!jumbleChars.ContainsKey(c) || wordChars[c] > jumbleChars[c])
+                    {
+                        break;
+                    }
+                }
+                if (word.Length == i)
+                {
+                    possibleWords.Add(word);
+                }
+            }
+
+            return possibleWords.ToArray();
         }
     }
 }
